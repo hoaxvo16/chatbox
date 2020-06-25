2ed44e1f9660d3adb5812b9ed29b9c127a99ddb7
VoHoa-HCMUS <vohoa1468@gmail.com>
2020-06-25 23:17:38 +0700

    make it better

---
+++ public/index.js
@@ -0,0 +1,62 @@
+var socket = io("http://localhost:3000");
+
+socket.on("username-taken", function () {
+  alert("username-taken!!!!");
+});
+
+socket.on("success-login", function (data) {
+  $("#hello-user").html(data);
+  $(".login-box").hide();
+  $("#chat-div").show();
+});
+
+socket.on("user-online-list", function (data) {
+  $("#online-user").html("");
+  data.forEach(element => {
+    $("#online-user").append("<div class='current-user'>" + element + "</div>");
+  });
+});
+
+socket.on("sbd-logout", function (data) {
+  $("#online-user").html("");
+  data.forEach(element => {
+    $("#online-user").append("<div class='current-user'>" + element + "</div>");
+  });
+});
+
+socket.on("server-send-mess", function (data) {
+  $("#chat-box").append(
+    "<div class='mess'><b>" + data.usn + "</b>: " + data.mess + "</div>"
+  );
+});
+
+$(document).ready(function () {
+  $(".login-box").show();
+  $("#chat-div").hide();
+
+  $("#signIn").click(function () {
+    socket.emit("username-send", $("#usn").val());
+  });
+
+  $("#log-out").click(function () {
+    socket.emit("logout");
+    $("#chat-div").hide();
+    $(".login-box").show();
+  });
+
+  $("#send-btn").click(function () {
+    socket.emit("user-send-mess", $("#chat-mess").val());
+    $("#chat-mess").val("");
+  });
+
+  $("#chat-mess").bind("enterKey", function (e) {
+    socket.emit("user-send-mess", $("#chat-mess").val());
+    $("#chat-mess").val("");
+  });
+
+  $("#chat-mess").keyup(function (e) {
+    if (e.keyCode == 13) {
+      $(this).trigger("enterKey");
+    }
+  });
+});
---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var users = [];
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+server.listen(3000);
+console.log("server is running");
+
+io.on("connection", function (socket) {
+  console.log("Connected with: " + socket.id);
+
+  socket.on("disconnect", function () {
+    console.log(socket.id + " disconnected");
+    users.splice(users.indexOf(socket.userName), 1);
+    socket.broadcast.emit("sbd-logout", users);
+  });
+
+  socket.on("username-send", function (data) {
+    if (users.indexOf(data) >= 0) {
+      socket.emit("username-taken");
+    } else {
+      users.push(data);
+      socket.userName = data;
+      console.log("user " + data + " has joined");
+      socket.emit("success-login", data);
+      io.sockets.emit("user-online-list", users);
+    }
+  });
+
+  socket.on("logout", function () {
+    users.splice(users.indexOf(socket.userName), 1);
+    console.log(socket.userName + " logged out");
+    socket.broadcast.emit("sbd-logout", users);
+  });
+
+  socket.on("user-send-mess", function (data) {
+    io.sockets.emit("server-send-mess", { usn: socket.userName, mess: data });
+  });
+});
+
+app.get("/", function (req, res) {
+  res.render("homepage");
+});
